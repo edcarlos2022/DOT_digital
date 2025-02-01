79c94f48f0543c385b0a97f8f973c0ae60df7b59
Ed Carlos <102836693+edcarlos2022@users.noreply.github.com>
2025-02-01 01:10:26 -0300

    alterações no cypress.config e yml

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,22 @@
 const { defineConfig } = require("cypress");
 
-module.exports = {
+module.exports = defineConfig({
+  viewportHeight: 880,
+  viewportWidth: 1280,
+  pageLoadTimeout: 130000,
+  defaultCommandTimeout: 10000, // Tempo máximo para encontrar elementos
+
   e2e: {
+    
+    setupNodeEvents(on, config) {
+      
+    },
     baseUrl: 'https://loja.vr.com.br/',
-    specPattern: 'cypress/integration/**/*.spec.js'
+    specPattern: 'cypress/e2e/**/*.cy.js', 
+    retries: {
+      runMode: 2,  // Tentativas em execução dos testes
+      openMode: 1  // Tentativas quando o Cypress é aberto em modo interativo
+    },
   }
-}
+});
+
